60a88d2c75b92f82222645839f871a211a7c5cf0
Tuan Anh <victory1080@gmail.com>
2021-03-08 21:04:53 +0700

    Updates

---
+++ precache-manifest.d40e52a32311f15d9efc064387ab7623.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b19d4fa1e01df5f1765afd8707b94882",
+    "url": "/Chapter_18_DeployReact/index.html"
+  },
+  {
+    "revision": "68aa0ac837664873044f",
+    "url": "/Chapter_18_DeployReact/static/css/2.80fa2b61.chunk.css"
+  },
+  {
+    "revision": "68aa0ac837664873044f",
+    "url": "/Chapter_18_DeployReact/static/js/2.493fab9e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Chapter_18_DeployReact/static/js/2.493fab9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "586ca7798664f70bdc06",
+    "url": "/Chapter_18_DeployReact/static/js/main.dc518724.chunk.js"
+  },
+  {
+    "revision": "8f59357f6019dc475550",
+    "url": "/Chapter_18_DeployReact/static/js/runtime-main.166efe32.js"
+  }
+]);
